da5a5cfd2c8f91bfd6822d12745c4d85d406f5c9
Thamsanqa Makhubu <makhubu130700@gmail.com>
2025-06-29 15:36:40 +0200

    Add files via upload

---
+++ python.py
@@ -0,0 +1,8 @@
+#Loop Control Statements
+
+count = 0
+while count < 5:
+    print(count)
+    count += 1
+    if count == 3:
+        break  # Exit the loop when count is 3
---
+++ strings.py
@@ -0,0 +1,9 @@
+#Advanced concepts - Strings
+
+message = '  Hello, World!  '
+
+print(message.strip()) # Remove leading and trailing whitespace
+print(message.lower()) # Convert to lowercase
+print(message.split(',')) # Split the string by comma
+
+#
