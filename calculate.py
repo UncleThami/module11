b5ac950b1c45a9fa45436fe493c0a934397e97d4
Thamsanqa Makhubu <makhubu130700@gmail.com>
2025-07-01 00:33:16 +0200

    Add files via upload

---
+++ calculate.py
@@ -0,0 +1,8 @@
+
+def  area(lenght, width):
+    #Function to calculate the area of a rectangle
+    return lenght * width
+
+def perimeter(lenght, width):
+    #Function to calculate the perimeter of a rectangle
+    return 2 * (lenght + width)
